b2c9491b69546c7756dc2aa3d0ec44d57c0127a5
Samuel Pires <samuelp2002@gmail.com>
2022-11-01 17:16:56 +0000

    added ActionListener to menu items

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		frame = new GraphPlotterFrame();
 		frame.drawReferential();
+		frame.drawFunction();
 	}
 }
--- src/graphplotter/GraphPlotterFrame.java
+++ src/graphplotter/GraphPlotterFrame.java
@@ -1,16 +1,22 @@
 package graphplotter;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 @SuppressWarnings("serial")
-public class GraphPlotterFrame extends JFrame {
+public class GraphPlotterFrame extends JFrame implements ActionListener {
 	
 	private JMenu menuVW, menuGS;
 	private JMenuItem vwDefault, vwSetValues, vwZoomIn, vwZoomOut;
 	private JMenuItem gsRoot, gsMax, gsMin, gsYIntersect, gsIntersect, gsYCalc, gsXCalc, gsIntegral;
+	
+	ReferentialGraphic rg;
+	FunctionGraphic fg;
 	
 
 	public GraphPlotterFrame() {
@@ -23,23 +29,20 @@ public class GraphPlotterFrame extends JFrame {
 	    this.setResizable(false);
 	    this.setVisible(true);
 	    
-		addMenu();
+		addMenuBar();
 	}
 	
-	private void addMenu() {
+	private void addMenuBar() {
 		JMenuBar menubar = new JMenuBar();
 		
 	    menuVW = new JMenu("View Window");
+	    menuGS = new JMenu("G-Solve");
+	    
 	    vwDefault = new JMenuItem("Default");
 	    vwSetValues = new JMenuItem("Set Values");
 	    vwZoomIn = new JMenuItem("Zoom In");
 	    vwZoomOut = new JMenuItem("Zoom Out");
-	    menuVW.add(vwDefault);
-	    menuVW.add(vwSetValues);
-	    menuVW.add(vwZoomIn);
-	    menuVW.add(vwZoomOut);
 	    
-	    menuGS = new JMenu("G-Solve");
 	    gsRoot = new JMenuItem("Root");
 	    gsMax = new JMenuItem("Maximum");
 	    gsMin = new JMenuItem("Minimum");
@@ -48,6 +51,24 @@ public class GraphPlotterFrame extends JFrame {
 	    gsYCalc = new JMenuItem("Y-Value");
 	    gsXCalc = new JMenuItem("X-Value");
 	    gsIntegral = new JMenuItem("Integral");
+	    
+	    vwDefault.addActionListener(this);
+	    vwSetValues.addActionListener(this);
+	    vwZoomIn.addActionListener(this);
+	    vwZoomOut.addActionListener(this);
+	    gsRoot.addActionListener(this);
+	    gsMax.addActionListener(this);
+	    gsMin.addActionListener(this);
+	    gsYIntersect.addActionListener(this);
+	    gsIntersect.addActionListener(this);
+	    gsYCalc.addActionListener(this);
+	    gsXCalc.addActionListener(this);
+	    gsIntegral.addActionListener(this);
+	    
+	    menuVW.add(vwDefault);
+	    menuVW.add(vwSetValues);
+	    menuVW.add(vwZoomIn);
+	    menuVW.add(vwZoomOut);    
 	    menuGS.add(gsRoot);
 	    menuGS.add(gsMax);
 	    menuGS.add(gsMin);
@@ -62,10 +83,69 @@ public class GraphPlotterFrame extends JFrame {
 	    this.setJMenuBar(menubar);
 	}
 	
-	
 	public void drawReferential() {
-		ReferentialGraphic rg = new ReferentialGraphic();
+		rg = new ReferentialGraphic();
 		this.add(rg);
 		this.setVisible(true);
+	}
+	
+	public void drawFunction() {
+		fg = new FunctionGraphic();
+		this.add(fg);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == vwDefault) {
+			
+		}
+		
+		if(e.getSource() == vwSetValues) {
+			
+		}
+		
+		if(e.getSource() == vwZoomIn) {
+			
+		}
+		
+		if(e.getSource() == vwZoomOut) {
+			
+		}
+		
+		if(e.getSource() == gsRoot) {
+			
+		}
+		
+		if(e.getSource() == gsMax) {
+			
+		}
+		
+		if(e.getSource() == gsMin) {
+			
+		}
+		
+		if(e.getSource() == gsYIntersect) {
+			
+		}
+		
+		if(e.getSource() == gsIntersect) {
+			
+		}
+		
+		if(e.getSource() == gsYCalc) {
+			
+		}
+		
+		if(e.getSource() == gsXCalc) {
+			
+		}
+		
+		if(e.getSource() == gsIntegral) {
+			
+		}
+		
+		
 	}
 }
